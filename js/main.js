63308efff82c7c5c830d42151193f3aeeb979459
SSPandaSS <boss.rinat.98@mail.ru>
2023-05-29 21:54:53 +0500

    feat: add header/ slider section / search section

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
 $(function(){
+    $('.banner-section__slider').slick({
+        dots: true,
+        prevArrow: '<button class="banner-section__slider-btn banner-section__slider-prev"><img src="images/arrow-left.svg" alt="prev"></button>',
+        nextArrow: '<button class="banner-section__slider-btn banner-section__slider-next"><img src="images/arrow-right.svg" alt="next"></button>'
+    });
 
+    $('.search__tabs-item').on('click', function(e) {
+        e.preventDefault();
+        
+        $('.search__tabs-item').removeClass('search__tab-active');
+        $('.search__content-item').removeClass('search__content-active');
+
+        $(this).addClass('search__tab-active');
+        $($(this).attr('href')).addClass('search__content-active');
+    });
 });
